d956e609d5bdd7ccf291b836ce27825df5338c78
leap <leapss@outlook.com>
2024-12-10 17:04:47 +0800

    数据库连接成功

---
+++ BookManagement/entity/Book.cs
@@ -0,0 +1,20 @@
+﻿using SqlSugar;
+
+namespace BookManagement.entity
+{
+    [SugarTable("book")] // 表名与数据库中的表名对应
+    public class Book
+    {
+        [SugarColumn(IsPrimaryKey = true)] // 主键，自动增长
+        public string isbn { get; set; }
+        public string author { get; set; }
+        public string bookName { get; set; }
+        public string press { get; set; }
+        public string pressDate { get; set; }
+        public string picture { get; set; }
+        public string clcName { get; set; }
+        public int price { get; set; }
+        public string bookDesc { get; set; }
+        public string language { get; set; }
+    }
+}
---
+++ BookManagement/util/DatabaseService.cs
@@ -0,0 +1,56 @@
+﻿using SqlSugar;
+using System;
+
+namespace BookManagement.util
+{
+    class DatabaseService
+    {
+        private static DatabaseService _instance;
+        private static readonly object _lock = new object();
+        private SqlSugarClient _db;
+
+        private DatabaseService()
+        {
+            _db = new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = "Server=52.194.237.192;Port=32750;Database=book;Uid=postgres;Pwd=9#327.5",
+                DbType = DbType.PostgreSQL,
+                IsAutoCloseConnection = true,
+                InitKeyType = InitKeyType.Attribute
+            });
+
+            _db.CurrentConnectionConfig.ConfigureExternalServices = new ConfigureExternalServices
+            {
+                EntityService = (property, column) =>
+                {
+                    // 属性名驼峰 -> 列名下划线
+                    column.DbColumnName = string.Concat(property.Name.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())).ToLower();
+                }
+            };
+
+
+            // 打印SQL日志（可选）
+            _db.Aop.OnLogExecuting = (sql, pars) =>
+            {
+                Console.WriteLine(sql); // 输出SQL语句到控制台
+            };
+        }
+
+        public static DatabaseService Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DatabaseService();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public SqlSugarClient Db => _db;
+    }
+}
