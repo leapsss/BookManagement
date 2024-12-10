a26bad084ce7854e0c4af038306433c8ff0e79db
leap <leapss@outlook.com>
2024-12-10 16:19:23 +0800

    添加项目文件。

---
+++ BookManagement/page/a.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+
+namespace BookManagement.page
+{
+    /// <summary>
+    /// a.xaml 的交互逻辑
+    /// </summary>
+    public partial class a : Window
+    {
+        public a()
+        {
+            InitializeComponent();
+        }
+    }
+}
