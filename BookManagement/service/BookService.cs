d5ac65d0550b1224a6976c53b1dfc6538e4a2022
Chenyang <hellowdcy16@gmail.com>
2024-12-17 16:56:23 +0800

    created book service methods

--- BookManagement/service/BookService.cs
+++ BookManagement/service/BookService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookManagement.entity;
+using BookManagement.repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,28 @@ using System.Threading.Tasks;
 
 namespace BookManagement.service
 {
-    internal class BookService
+    public class BookService
     {
+        private readonly BookRepository bookRepository;
+        public BookService()
+        {
+            bookRepository = new BookRepository();
+        }
+
+        public void Add(Book book)
+        {
+            bookRepository.Add(book);
+        }
+
+        public Book GetByISBN(string isbn)
+        {
+            return bookRepository.GetByISBN(isbn);
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return bookRepository.GetAll();
+        }
 
     }
 }
